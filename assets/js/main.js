d408292423f12062d738e79745016e78116ea92b
Le Mien <lemien@Les-MacBook-Air.local>
2022-07-27 10:40:45 +0700

    Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,8 +61,8 @@
             }
         });
 
-        if(isMobile){
-            var header__navigation = $('.header__navigation .nav-item');
+        if(isMobile()){
+            var header__navigation = $('.header__navigation');
             header__navigation.each(function () {
                 var button = $(this).find('a');
                 button.click(function () {
@@ -74,23 +74,6 @@
             });
         }
 
-        // Home load animation
-        if($('body.home-load-animation').length) {
-            var nav_item = $('.header__navigation .nav-item');
-            nav_item.each(function () {
-                var button = $(this).find('a');
-                if(!button.hasClass('active')) {
-                    var section = button.attr('href');
-                    $(section).css('display', 'none');
-                }
-                button.click(function () {
-                    setTimeout(function () {
-                        $('body').removeClass('nav-open-js');
-                        button_nav.removeClass('active');
-                    },600);
-                });
-            });
-        }
     }
 
     var lazy = function () {
